8f28923bd978c0dd5b938d032dceb532d5813df0
Danny <dz37@rice.edu>
2023-08-21 17:44:54 -0500

    Initial Commit

---
+++ Pokedex-Verkada/Pokedex_VerkadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_VerkadaApp.swift
+//  Pokedex-Verkada
+//
+//  Created by Danny Zhang on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_VerkadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
